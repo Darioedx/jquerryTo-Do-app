3405cf58392b1f517576f8b6baa4e738af552c50
Darioedx <dariovicentealmiron@gmail.com>
2023-03-02 07:05:52 +0100

    app completed

--- js/script.js
+++ js/script.js
@@ -2,34 +2,28 @@
 
 function newItem(){
   let li = $('<li></li>');
+  
   let doneButton = $('<crossOutButton></crossOutButton>')
   if ($('#input').val() === '') {
     alert("You must write something!");
   } else {
       $('#list').append(li.append($('#input').val()));
       li.append(doneButton.append(document.createTextNode('X')));
+      
   } 
+  
+  li.on("dblclick", function crossOut() {
+      li.toggleClass("strike");
+    });
 
+  
   doneButton.on("click", deleteListItem)
   function deleteListItem(){
     
       li.addClass("delete");
     } 
+    $('#list').sortable();
+   
+
   }   
-
-
-        ///C.F given code/////
-
-//let li = $('<li></li>');
-//let inputValue = $('#input').val();
-//li.append(inputValue);
-
-//if (inputValue === '') {
-  //alert("You must write something!");
-//} else {
-  //$('#list').append(li);
-//}
-//crossOutButton.on("click", deleteListItem);
-//   function deleteListItem(){
-// 		li.addClass("delete")
-//<input type="checkbox"></input>
+  
